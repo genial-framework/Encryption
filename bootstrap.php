b7537197266f796823e10b68fe059aa2a8e2918b
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2017-12-11 15:50:43 -0500

    Update bootstrap.php
    Add some hash constants to help the Genial\Encryption\Algo class.

--- bootstrap.php
+++ bootstrap.php
@@ -1,1 +1,9 @@
+<?php
+/*
+ * @link      <https://github.com/Genial-Framework/Encryption> for the canonical source repository
+ * @copyright Copyright (c) 2017-2018 Genial Framework. <https://github.com/Genial-Framework>
+ * @license   <https://github.com/Genial-Framework/Encryption/blob/master/LICENSE> New BSD License
+ */
 
+define('REQUEST_HASH_ALGOS', 0);
+define('REQUEST_HASH_HMAC_ALGOS', 1);
