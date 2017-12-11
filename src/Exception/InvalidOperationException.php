572dc5da65d95d46de3fe9c9acce2800e3013f6f
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2017-12-11 15:33:20 -0500

    Create InvalidOperationException.php

---
+++ src/Exception/InvalidOperationException.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * @link      <https://github.com/Genial-Framework/Encryption> for the canonical source repository
+ * @copyright Copyright (c) 2017-2018 Genial Framework. <https://github.com/Genial-Framework>
+ * @license   <https://github.com/Genial-Framework/Encryption/blob/master/LICENSE> New BSD License
+ */
+ 
+namespace Genial\Encryption\Exception;
+
+/**
+ * InvalidOperationException
+ */
+class InvalidOperationException extends RuntimeException implements EncryptionExceptionInterface, ExceptionInterface
+{
+}
